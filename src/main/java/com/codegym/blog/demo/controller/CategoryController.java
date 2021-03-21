fc71f190fce428555d05ba6fbfb81da419945673
new-biee <anhvtth1907004@fpt.edu.vn>
2021-03-22 01:32:22 +0700

    update

--- src/main/java/com/codegym/blog/demo/controller/CategoryController.java
+++ src/main/java/com/codegym/blog/demo/controller/CategoryController.java
@@ -24,7 +24,7 @@ public class CategoryController {
         return categoryService.categoryList();
     }
 
-    @PostMapping
+    @PostMapping("/create")
     public ResponseEntity<SystemResponse<Category>> createCategory(@RequestBody Category category) {
         return categoryService.createCategory(category);
     }
